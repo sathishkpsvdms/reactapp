8dcb30b531690ed1229ebb06d8bdd0585e59fd40
SathishKumaran <Sathishkumaran@test.com>
2018-02-07 12:16:03 +0530

    Added deploy PS script

---
+++ reactdeploy.ps1
@@ -0,0 +1,20 @@
+﻿Set-StrictMode -Version 'Latest'
+$ErrorActionPreference = 'Stop'
+$VerbosePreference = 'Continue'
+
+#Declare variables
+$Builddir= "%teamcity.build.checkoutDir%\build"
+$deploypath = "C:\inetpub\wwwroot\reactapp\"
+$GetFile = get-childitem -file -recurse -name $Builddir
+$Devserver = "dev2-app.united.com"
+
+$Filenames = foreach ($FileName in $GetFile)
+{
+
+cd $Builddir
+
+#Deploy files
+write-output "Copying $FileName to Folder $deploypath"
+Copy-Item $FileName $deploypath -force -Recurse
+}
+
